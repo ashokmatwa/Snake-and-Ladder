88c402bbbbb5e757da03f2a9b00c89efe214ddc0
Ashok Matwa <ashokmatwa003@gmail.com>
2022-12-23 12:40:36 +0530

    ..

--- src/main/java/com/example/snakeandladder/Player.java
+++ src/main/java/com/example/snakeandladder/Player.java
@@ -10,13 +10,13 @@ public class Player {
 
     private int coinPosition;
 
-    public Player(int tileSize, Color coinColor, String playerName){
+    public Player(int tileSize, Color coinColor, String playerName){ //constructor
         coinPosition = 1;  // game starts with one
         name = playerName;
         coin = new Circle(tileSize/2);
         coin.setFill(coinColor);
-        coin.setTranslateX(20);
-        coin.setTranslateY(380);
+        coin.setTranslateX(20);   // coin size =20 so put it on its centre 0+20
+        coin.setTranslateY(380);  // 400-20 = 380 for y axis
     }
 
     public Circle getCoin() {
